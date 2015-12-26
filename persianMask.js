f96ed72b59830267878a284c78b852d7c046fc90
jalalazimi@gmail.com <jalalazimi@gmail.com>
2015-12-26 11:46:20 +0330

    PerisanMask

--- persianMask.js
+++ persianMask.js
@@ -1,36 +1,123 @@
-(function($) {
-$.fn.persianSeprator = function(price) {
-setComma = function(number) {
-    number = "" + number,
-    number = persianInt(number);
-    if (number)
-        while (/(\d+)(\d{3})/.test(number))
-            number = number.replace(/(\d+)(\d{3})/, "$1,$2");
-    return number
-},
-persianInt = function(number) {
-    var numberDictionary = {
-        "۱": "1",
-        "۲": "2",
-        "۳": "3",
-        "۴": "4",
-        "۵": "5",
-        "۶": "6",
-        "۷": "7",
-        "۸": "8",
-        "۹": "9",
-        "۰": "0",
-        "/": "."
+/*
+ * PersianInputMask v0.1.0
+ * https://github.com/jalalazimi/persianMask.js
+ * MIT licensed
+ *
+ * Copyright (C) 2015 Jalal azimi
+ */
+
+(function ($) {
+
+    var VERSION = "0.1.0",
+        //Check for nodeJS
+        hasModule = (typeof module !== 'undefined' && module.exports);
+
+    //PersianJs main function/constructor, used for prototype.
+    function PersianMask(str) {
+        this._str = str;
     }
-      , result = "";
-    if (number != null  && number != undefined) {
-        var numberString = number.toString();
-        for (var i = 0; i < numberString.length; i++) {
-            var setNumber = numberDictionary[numberString.charAt(i)];
-            setNumber ? result += setNumber : result += numberString.charAt(i)
+
+    /**
+     * Used for set comma
+     *
+     * @param {Number} value
+     * @return {String} Returns Converted string
+     * @api private
+     */
+    function _setComma(value) {
+        if (!value) {
+            return;
         }
+        value = "" + value;
+            value = _persianInt(value);
+        if (value)
+            while (/(\d+)(\d{3})/.test(value))
+                value = value.replace(/(\d+)(\d{3})/, "$1,$2");
+        return value
     }
-    return result
-}
-}
-})(jQuery);
+
+    /**
+     * Used for convert persian number to English number
+     *
+     * @param {String} value
+     * @return {String} Returns Converted string
+     * @api private
+     */
+    function _persianInt(value) {
+        var numberDictionary = {
+                "۱": "1",
+                "۲": "2",
+                "۳": "3",
+                "۴": "4",
+                "۵": "5",
+                "۶": "6",
+                "۷": "7",
+                "۸": "8",
+                "۹": "9",
+                "۰": "0",
+                "/": "."
+            }
+            , result = "";
+        if (value != null && value != undefined) {
+            var numberString = value.toString();
+            for (var i = 0; i < numberString.length; i++) {
+                var setNumber = numberDictionary[numberString.charAt(i)];
+                setNumber ? result += setNumber : result += numberString.charAt(i)
+            }
+        }
+        return result
+    }
+
+
+
+    var persianMask = function (inputStr) {
+        if (inputStr == "" || inputStr == null) {
+            return null;
+        }
+        return new PersianMask(inputStr);
+    }
+
+    //Version
+    persianMask.version = VERSION;
+
+    //Prototype
+    persianMask.fn = PersianMask.prototype = {
+        clone: function () {
+            return persianMask(this);
+        },
+        value: function () {
+            return this._str;
+        },
+        toString: function () {
+            return this._str.toString();
+        },
+        set : function (value) {
+            this._str = String(value);
+            return this;
+        },
+        setComma: function () {
+            return _setComma(this);
+        }
+    };
+
+    //Expose persianMask
+    //CommonJS module is defined
+    if (hasModule) {
+        module.exports = persianMask;
+    }
+    //global ender:false
+    if (typeof ender === 'undefined') {
+        // here, `this` means `window` in the browser, or `global` on the server
+        // add `persianMask` as a global object via a string identifier,
+        // for Closure Compiler "advanced" mode
+        this['persianMask'] = persianMask;
+    }
+    //global define:false
+    if (typeof define === 'function' && define.amd) {
+        define('persianMask', [], function () {
+            return persianMask;
+        });
+    }
+
+
+})();
