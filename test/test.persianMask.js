82c70c3af3fca0d303277cb81d51911986c59a9b
jalalazimi@gmail.com <jalalazimi@gmail.com>
2015-12-26 13:42:23 +0330

    test

--- test/test.persianMask.js
+++ test/test.persianMask.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 
 describe('convert', function () {
     it('should set comma separator on Persian numbers', function (done) {
-        assert.equal("???????", persianMask("1,129,200").setComma());
+        assert.equal("1,129,200", persianMask("۱۱۲۹۲۰۰").setComma());
         done();
     });
 });
