6f5c0567098a73c5663363a109bff7aa5e438912
jalalazimi@gmail.com <jalalazimi@gmail.com>
2015-12-28 17:06:49 +0330

    create toPersian and toEnglish function

--- persianMask.js
+++ persianMask.js
@@ -13,9 +13,9 @@
         hasModule = (typeof module !== 'undefined' && module.exports);
 
 
-    var arabicNumbers = ["١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩", "٠"],
-        persianNumbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۰"],
-        englishNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    var arabicNumbers = ["٠", "١", "٢", "٣", "٤", "٥", "٦", "٧", "٨", "٩"],
+        persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"],
+        englishNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
         persianChar = ["ض", "ص", "ث", "ق", "ف", "غ", "ع", "ه", "خ", "ح", "ج", "چ", "ش", "س", "ی", "ب", "ل", "ا", "ت", "ن", "م", "ک", "گ", "ظ", "ط", "ز", "ر", "ذ", "د", "پ", "و", "؟"],
         englishChar = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", "?"],
         arabicChars = ["ي", "ك", "‍", "دِ", "بِ", "زِ", "ذِ", "ِشِ", "ِسِ", "‌", "ى"],
@@ -36,43 +36,73 @@
      */
     function _setComma(value) {
         if (!value) return;
-        value = _persianNumber(value);
+        value = _toEnglish(value);
         while (/(\d+)(\d{3})/.test(value))
             value = value.replace(/(\d+)(\d{3})/, "$1,$2");
         return value
     }
 
     /**
-     * Used for convert persian number to English number
+     * Used for convert Persian to English number
      *
      * @param {String} value
      * @return {String} Returns Converted string
      * @api private
      */
-    function _persianNumber(value) {
+    function _toEnglish(value) {
         if (!value) return;
-        var numberDictionary = {
-                "۱": "1",
-                "۲": "2",
-                "۳": "3",
-                "۴": "4",
-                "۵": "5",
-                "۶": "6",
-                "۷": "7",
-                "۸": "8",
-                "۹": "9",
-                "۰": "0",
-                "/": "."
-            },
-            result = "";
-        var numberString = value.toString();
-        for (var i = 0; i < numberString.length; i++) {
-            var setNumber = numberDictionary[numberString.charAt(i)];
-            setNumber ? result += setNumber : result += numberString.charAt(i)
+        var value = value.toString();
+        for (var i = 0, len = value.length; i < len; i++) {
+            var index = _ArrayIndex(persianNumbers,value[i]);
+            if (index > -1)
+                value = _searchAndReplace(value,i,index);
         }
-        return result
+        return value
     }
 
+    /**
+     * Used for convert English to Persian number
+     *
+     * @param {String} value
+     * @return {String} Returns Converted string
+     * @api private
+     */
+    function _toPersian(value) {
+        if (!value) return;
+        var value = value.toString();
+        for (var i = 0, len = value.length; i < len; i++) {
+            var index = _ArrayIndex(englishNumbers, value[i]);
+            if (index > -1)
+                value = _searchAndReplace(value, i, persianNumbers[index]);
+        }
+        return value
+    }
+
+
+    /**
+     * Used for return Array Index
+     *
+     * @param {String} value
+     * @return {String} Returns array Index
+     * @api private
+     */
+    function _ArrayIndex(array, value) {
+        return array.indexOf(value)
+    }
+
+
+    /**
+     * Used for search in Array and replace
+     *
+     * @param {String} value
+     * @param {String} array index
+     * @param {String} new value
+     * @return {String} Returns replaced value
+     * @api private
+     */
+    function _searchAndReplace(value, index, newValue) {
+        return value.replace(new RegExp(value[index], "g"), newValue)
+    }
 
 
 
@@ -102,8 +132,20 @@
         },
         setComma: function() {
             return _setComma(this);
+        },
+        toPersian: function() {
+            return _toPersian(this);
+        },
+        toEnglish: function() {
+            return _toEnglish(this);
         }
     };
+
+
+    String.prototype.setComma = function() {
+        return _setComma(this);
+    }
+
 
     //CommonJS module
     if (hasModule) {
